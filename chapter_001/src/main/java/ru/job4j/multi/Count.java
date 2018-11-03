2853b0ed8a85b40cc9f13f75e93598a2b7208862
warham194 <warham194@gmail.com>
2018-11-03 13:41:35 +0300

    класс Count и метод int incremant() [#1102]

---
+++ chapter_001/src/main/java/ru/job4j/multi/Count.java
@@ -0,0 +1,21 @@
+package ru.job4j.multi;
+
+/**
+ * Created by Lenovo2 on 02.11.2018.
+ */
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
+public class Count {
+    @GuardedBy("this")
+    private int value;
+
+    public synchronized void increment() {
+        this.value++;
+    }
+
+    public synchronized int get() {
+        return this.value;
+    }
+}
