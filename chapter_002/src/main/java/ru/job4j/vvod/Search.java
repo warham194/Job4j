72f5c3c28859a7f364de4205ebb1e60d01253780
warham194 <warham194@gmail.com>
2019-03-12 17:06:24 +0300

    Правки архивирования

--- chapter_002/src/main/java/ru/job4j/vvod/Search.java
+++ chapter_002/src/main/java/ru/job4j/vvod/Search.java
@@ -13,33 +13,34 @@ public class Search {
     public List<File> files(String parent, List<String> exts) {
         ArrayList<File> result = new ArrayList<>();
         Queue<File> data = new LinkedList<>();
-
         try {
-            for (File file : Arrays.asList(new File(parent).listFiles())) {
-                data.add(file);
-            }
-
-            File useFile;
+            data.add(new File(parent));
             while (!data.isEmpty()) {
-                useFile = data.poll();
+                File useFile = data.poll();
                 if (useFile.isDirectory()) {
                     for (File f : Arrays.asList(useFile.listFiles())) {
                         data.add(f);
                     }
                 } else {
-                    for (String s : exts) {
+                    result.add(addFileWithEx(exts,useFile));
+                   /* for (String s : exts) {
                         if (useFile.getName().endsWith(s)) {
                             result.add(useFile);
                         }
-                    }
+                    }*/
                 }
             }
-
-
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         return result;
+    }
+
+    public File addFileWithEx(List<String> exceptions, File file) {
+        for (String s : exceptions) {
+            if (file.getName().endsWith(s))
+                return file;
+        }
+        return null;
     }
 }
