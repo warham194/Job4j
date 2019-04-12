91ba7abebe9e3617c18eca518d8fdfe856f4f920
warham194 <warham194@gmail.com>
2019-04-12 16:59:19 +0300

    Фильтр

---
+++ chapter_003/src/main/java/ru/job4j/sql/filter.sql
@@ -0,0 +1,50 @@
+--\connect requests
+
+--\connect requests
+
+create table typeProduct (
+	id serial primary key,
+	name varchar(1000)
+);
+create table product (
+	id serial primary key,
+	name varchar(1000),
+	type_id integer references typeProduct(id),
+	expired_date timestamptz,
+	price money
+);
+
+--1. Написать запрос получение всех продуктов с типом "СЫР"
+select * from typeProduct as t
+inner join product as p on t.id = p.type_id
+where t.name = 'СЫР';
+--2. Написать запрос получения всех продуктов, у кого в имени есть слово "мороженное"
+select * from product as p
+where p.name like '%мороженное%';
+
+
+--не нашел в документаци , как задать дату
+--3. Написать запрос, который выводит все продукты, срок годности которых заканчивается в следующем месяце.
+--select * from product as p
+--where p.expired_date = expired_date
+
+
+
+--4. Написать запрос, который выводит самый дорогой продукт.
+select * from product as p
+where GREATEST(p.price);
+--5. Написать запрос, который выводит количество всех продуктов определенного типа.
+select count(p.id)
+from product p inner join type t on t.id = p.type_id
+where t.name = 'СЫР';
+--6. Написать запрос получение всех продуктов с типом "СЫР" и "МОЛОКО"
+select * from typeProduct as t
+inner join product as p on t.id = p.type_id
+where t.name in ('СЫР', 'МОЛОКО');
+--7. Написать запрос, который выводит тип продуктов, которых осталось меньше 10 штук.
+select t.name, p.name
+from type t inner join product p on t.id = p.type_id
+where p.quantity < 10;
+--8. Вывести все продукты и их тип.
+select * from typeProduct as t
+inner join product as p on t.id = p.type_id;
